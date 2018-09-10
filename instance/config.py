0051dc3fe1b558fee7ba157b1b28ff585010fa70
H-Rigga <42773885+H-Rigga@users.noreply.github.com>
2018-09-10 13:28:38 +0300

    add instance folder

---
+++ instance/config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config(object):
+    pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
